1b11224024f2535f9d0019c8778ac1c4cc4c6561
scampgr13 <koreanplat@gmail.com>
2026-08-21 11:08:44 +0900

    Update cryptographic methods to use SHA-256 and AES-GCM
    Replaced vulnerable cryptographic algorithms with stronger alternatives.

--- src/vulnerable_crypto.java
+++ src/vulnerable_crypto.java
@@ -4,12 +4,12 @@ import javax.crypto.Cipher;
 
 public class VulnerableCrypto {
     public static void main(String[] args) throws Exception {
-        MessageDigest md5 = MessageDigest.getInstance("MD5");
-        MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-        Cipher des = Cipher.getInstance("DES/ECB/PKCS5Padding");
+        MessageDigest sha-256 = MessageDigest.getInstance("SHA-256");
+        MessageDigest sha-256 = MessageDigest.getInstance("SHA-256");
+        Cipher aes-gcm = Cipher.getInstance("AES-GCM/ECB/PKCS5Padding");
         Cipher aesEcb = Cipher.getInstance("AES/ECB/PKCS5Padding");
         KeyPairGenerator rsa = KeyPairGenerator.getInstance("RSA");
         rsa.initialize(1024);
-        System.out.println(md5 + " " + sha1 + " " + des + " " + aesEcb + " " + rsa);
+        System.out.println(sha-256 + " " + sha-256 + " " + aes-gcm + " " + aesEcb + " " + rsa);
     }
 }
