1b057c8f657dbec7a245756f49ac7c128055a973
scampgr13 <koreanplat@gmail.com>
2026-08-21 11:15:59 +0900

    Update vulnerable_crypto.java

--- src/vulnerable_crypto.java
+++ src/vulnerable_crypto.java
@@ -4,12 +4,21 @@ import javax.crypto.Cipher;
 
 public class VulnerableCrypto {
     public static void main(String[] args) throws Exception {
-        MessageDigest sha-256 = MessageDigest.getInstance("SHA-256");
-        MessageDigest sha-256 = MessageDigest.getInstance("SHA-256");
-        Cipher aes-gcm = Cipher.getInstance("AES-GCM/ECB/PKCS5Padding");
-        Cipher aesEcb = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
+        MessageDigest sha256_2 = MessageDigest.getInstance("SHA-256");
+
+        Cipher aesGcm = Cipher.getInstance("AES/GCM/NoPadding");
+        Cipher aesGcm2 = Cipher.getInstance("AES/GCM/NoPadding");
+
         KeyPairGenerator rsa = KeyPairGenerator.getInstance("RSA");
         rsa.initialize(2048);
-        System.out.println(sha-256 + " " + sha-256 + " " + aes-gcm + " " + aesEcb + " " + rsa);
+
+        System.out.println(
+            sha256 + " " +
+            sha256_2 + " " +
+            aesGcm + " " +
+            aesGcm2 + " " +
+            rsa
+        );
     }
 }
