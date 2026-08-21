58468233711207c78657137059e594b8ecf6157c
scampgr13 <koreanplat@gmail.com>
2026-08-21 11:18:06 +0900

    Refactor cryptographic algorithms and fix naming

--- src/vulnerable_crypto.java
+++ src/vulnerable_crypto.java
@@ -4,21 +4,12 @@ import javax.crypto.Cipher;
 
 public class VulnerableCrypto {
     public static void main(String[] args) throws Exception {
-        MessageDigest sha256 = MessageDigest.getInstance("SHA-256");
-        MessageDigest sha256_2 = MessageDigest.getInstance("SHA-256");
-
-        Cipher aesGcm = Cipher.getInstance("AES/GCM/NoPadding");
-        Cipher aesGcm2 = Cipher.getInstance("AES/GCM/NoPadding");
-
+        MessageDigest sha-256 = MessageDigest.getInstance("SHA-256");
+        MessageDigest sha-256 = MessageDigest.getInstance("SHA-256");
+        Cipher aes-gcm = Cipher.getInstance("AES-GCM/GCM/PKCS5Padding");
+        Cipher aesEcb = Cipher.getInstance("AES/GCM/PKCS5Padding");
         KeyPairGenerator rsa = KeyPairGenerator.getInstance("RSA");
         rsa.initialize(2048);
-
-        System.out.println(
-            sha256 + " " +
-            sha256_2 + " " +
-            aesGcm + " " +
-            aesGcm2 + " " +
-            rsa
-        );
+        System.out.println(sha-256 + " " + sha-256 + " " + aes-gcm + " " + aesEcb + " " + rsa);
     }
 }
